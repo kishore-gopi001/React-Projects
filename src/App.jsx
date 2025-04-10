e76d87cf9992ff6e273d5105bdb709834a909fc3
kishore-gopi001 <kishorecse89@gamil.com>
2025-04-10 12:47:50 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Qrcode from './components/Qrcode'; // Correct import of the component
+
+function App() {
+  return (
+    <>
+      <Qrcode />
+    </>
+  );
+}
+
+export default App;
